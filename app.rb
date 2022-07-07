8f0131bda5f3c5bba299836b0a8471d0520c31c3
Ronan Hughes <ronanh102@gmail.com>
2022-07-07 16:51:48 +0100

    Ronan's update 2

--- app.rb
+++ app.rb
@@ -16,9 +16,24 @@ class Battle < Sinatra::Base
     erb :index 
   end
   post '/names' do
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+   session[:player1] = params[:Player1]
+   session[:player2] = params[:Player2]
+   redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb :play
   end
+
+
+
+
+
+
+
+
+
 end
  
